60a4b5722208bcef1d5b6a8cc3c44f566d2bd46b
ryoh-y <r_yamafuji@outlook.jp>
2021-06-07 06:43:50 +0900

    0607

---
+++ slimproject/!dc.bat
@@ -0,0 +1,78 @@
+@echo off
+
+::************************************************************
+::File Name	: !dc.bat
+::Date		: 2021/06/04
+::作者		: yamafuji
+::
+::function	:tmpファイルをコピーします
+::************************************************************
+::変数:
+set CONTAINER=slimProject
+set IMAGE_NAME=slimimag
+::************************************************************
+echo.
+
+if "%1" =="" (
+	goto RARAM_ERROR
+) else if "%1" =="buildc" (
+	docker-compose build
+	goto CORRECT
+) else if "%1" =="up" (
+	docker-compose up -d
+	goto CORRECT
+) else if "%1" =="runc" (
+	docker-compose run
+	goto CORRECT
+) else if "%1" =="down" (
+	docker-compose down
+	goto CORRECT
+) else if "%1" =="ps" (
+	docker-compose ps %2
+	goto CORRECT
+) else if "%1" =="pull" (
+	docker pull %2
+	goto CORRECT
+)
+if not "%2" =="" (set CONTAINER="%2" )
+
+if "%1" =="build" (
+	docker build -t %IMAGE_NAME% .
+	goto CORRECT
+) else if "%1" =="exec" (
+	docker exec -it %CONTAINER% /bin/bash
+	goto CORRECT
+) else if "%1" =="execa" (
+	docker exec -it %CONTAINER% /etc/myinit
+	goto CORRECT
+) else if "%1" =="start" (
+	docker start %CONTAINER%
+	goto CORRECT
+) else if "%1" =="stop" (
+	docker stop %CONTAINER%
+	goto CORRECT
+) else if "%1" =="run" (
+set FILE_FULLDIR=%~dp0
+set FILE_DIR=%FILE_FULLDIR:\=/%
+set FILE_NAME=%FILE_DIR:~0,-1%
+
+	docker run -d -it -p 80:80 -p 3306:3306 --privileged ^
+	--sysctl net.ipv6.conf.all.disable_ipv6=1 --cap-add=NET_ADMIN ^
+	 -v %FILE_NAME%/data:/root/data ^
+	 -v %FILE_NAME%/install:/root/install ^
+	 --name %CONTAINER% %IMAGE_NAME% /sbin/init 
+	goto CORRECT
+)
+
+::----------------------------------------------------------
+:CORRECT
+exit /B
+
+:PARAM_ERROR
+echo パラメータエラー：ファイル名が必要です。
+exit /B
+
+:NOT_FOUND_ERROR
+echo ファイル、フォルダが見つかりません。
+exit /B
+
---
+++ slimproject/install.sh
@@ -0,0 +1,72 @@
+# ==================================================================
+#	date	: 2021/06/03
+#	author	: LINUX
+#	outline	: Linux CentOS Setup(yum)
+#	file	: install.sh
+# ==================================================================
+##PASS  -----------------
+ARCHIVE_DIR="/root/install"
+WORK_DIR="/usr/local/src"
+ROOT_DIR="/var/www/html"
+
+
+##LINUX BASIC -----------------
+timedatectl set-timezone Asia/Tokyo
+localectl set-locale LANG=ja_JP.utf8 
+localedef -f EUC-JP  -i ja_JP ja_JP.eucJP
+
+localectl set-locale LANG=ja_JP.utf8 
+source /etc/locale.conf 
+
+
+
+
+
+
+
+
+
+
+
+
+RUN source /etc/locale.conf 
+
+CMD ["httpd", "-D", "FOREGROUND"]
+
+LOG_DIR="$ARCHIVE_DIR/log"
+
+
+systemctl start httpd   #<--起動
+systemctl enable httpd   #<--自動起動
+
+
+timedatectl set-timezone Asia/Tokyo
+localedef -f UTF-8 -i ja_JP ja_JP
+localedef -f EUC-JP  -i ja_JP ja_JP.eucJP
+
+localectl set-locale LANG=ja_JP.utf8 
+source /etc/locale.conf 
+
+useradd XXX -M
+passwd XXX;
+
+#composer install
+cd $ROOT_DIR
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+cp /root/install/composer.json /var/www/html/composer.json
+php composer.phar install
+
+#composer slim download -----------------
+cp -r -f $ARCHIVE_DIR/ /var/www/html
+#cp -f /var/www/html/vendor/slim/slim/.htaccess /var/www/html/public/.htaccess
+
+
+### command slim act
+#php -S 0.0.0.0:8000
+
+#lsof -i:0.0.0.0:8000
+#ps ax | grep php
